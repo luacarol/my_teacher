76cd801fef9260d37d28fc47ffa264c1f7b01be8
luacarol <lucarolp.deoli@gmail.com>
2022-10-26 21:40:47 -0300

    Testando com uma foto do github

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from "next";
+import Lista from "../src/components/Lista/Lista";
 
 const Home: NextPage = () => {
   return (
     <>
-      
+      <Lista></Lista>
     </>
   )
 }
---
+++ frontend/src/components/Lista/Lista.tsx
@@ -0,0 +1,15 @@
+import { Foto, ItemLista, ListaStyled } from "./Lista.style";
+
+const Lista = () => {
+    return (
+        <>
+            <ListaStyled>
+                <ItemLista>
+                    <Foto src="https://github.com/luacarol.png"></Foto>
+                </ItemLista>
+            </ListaStyled>
+        </>
+    )
+}
+
+export default Lista;
